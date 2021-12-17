ff8856fc8175755abd030f0bafff62c4a75e7abb
Sam <samdehority@outlook.com>
2021-12-17 15:47:20 -0500

    Minor updates to logic for readability

--- LOGIC.js
+++ LOGIC.js
@@ -12,6 +12,12 @@ const tickers = {
 //INVESTMENT AMOUNT
 const amounts = [0.18, 0.22, 0.25, 0.25, 0.0, 0.1];
 
+//TEST SUM FUNC
+const reducer = (initialVal, valTwo) => initialVal + valTwo;
+
+//SUM OF TEST FUNC 
+const arrSum = amounts.reduce(reducer); 
+
 let childFunc = () => {
     for (let i = 0; i < investmentArr.length; i++) {
         var child = document.createElement("div");
@@ -24,6 +30,9 @@ let childFunc = () => {
 
 //ON-CLICK FUNCTION
 button.addEventListener("click", function () {
+    if (arrSum !== 1){
+        alert (`Please refactor your array, the sum is not 100% of your investment`)
+        } else {
     if (investmentArr.length > 0) {
         output.innerHTML = "";
         investmentArr = [];
@@ -32,13 +41,13 @@ button.addEventListener("click", function () {
     } else {
         investmentAmt(document.getElementById("investmentamount").value, amounts);
         childFunc();
-    }
+    }}
 });
 
 //CALCULATE INVESTMENT AMOUNTS
 function investmentAmt(cash, arr) {
     for (let i = 0; i < arr.length; i++) {
-        investmentArr.push(`${tickers[i]}${(cash * arr[i]).toFixed(2)}`);
+        investmentArr.push(`${tickers[i]}${(cash * arr[i]).toFixed(2)}` + " " + `(${amounts[i]* 100}`+`%)`);
     }
 }
 
